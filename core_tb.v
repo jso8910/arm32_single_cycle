8ca75849a21ead40200d5f4b7dd29b14927de893
Jason Rosenzweig <mail4jasonr@gmail.com>
2026-04-23 00:30:50 -0500

    Added binary search testbench

--- core_tb.v
+++ core_tb.v
@@ -15,88 +15,36 @@ module arm32_core_tb;
     always #5 clk = ~clk;
 
     // 4. Stimulus Block
+    integer prev_pc;
+    integer prev_prev_pc;
     initial begin
-        // Initialize signals
-        clk = 0;
-        rst = 0;
+        for (integer i = 0; i < 64; i++) begin
+            // Initialize signals
+            clk = 0;
+            rst = 0;
 
-        // Hold reset for a few cycles
-        #20;
-        rst = 1;
-        
-        $display("Reset de-asserted. Starting simulation...");
+            // Hold reset for a few cycles
+            #20;
+            rst = 1;
 
-        // Note: In a real ARM core, you'd load a program into memory here.
-        // For a "very basic" test, we can use 'hierarchical references' 
-        // to force values directly into your internal register file.
-        $readmemh("test.mem", uut.dram.MEM);
-        $readmemh("test.mem", uut.inst_mem.MEM);
+            $readmemh("workloads/binsearch/dmem.mem", uut.dram.MEM, 0, 255);
+            $readmemh("workloads/binsearch/imem.mem", uut.inst_mem.MEM, 0, 255);
 
-        @(posedge clk);
-        $display("%d:\t%d\t%d\t%d\t%d", uut.pc, uut.regfile_inst.regarray[0], uut.regfile_inst.regarray[1], uut.regfile_inst.regarray[2], uut.regfile_inst.regarray[14]);
-        @(posedge clk);
-        $display("%d:\t%d\t%d\t%d\t%d", uut.pc, uut.regfile_inst.regarray[0], uut.regfile_inst.regarray[1], uut.regfile_inst.regarray[2], uut.regfile_inst.regarray[14]);
-        @(posedge clk);
-        $display("%d:\t%d\t%d\t%d\t%d", uut.pc, uut.regfile_inst.regarray[0], uut.regfile_inst.regarray[1], uut.regfile_inst.regarray[2], uut.regfile_inst.regarray[14]);
-        @(posedge clk);
-        $display("%d:\t%d\t%d\t%d\t%d", uut.pc, uut.regfile_inst.regarray[0], uut.regfile_inst.regarray[1], uut.regfile_inst.regarray[2], uut.regfile_inst.regarray[14]);
-        @(posedge clk);
-        $display("%d:\t%d\t%d\t%d\t%d", uut.pc, uut.regfile_inst.regarray[0], uut.regfile_inst.regarray[1], uut.regfile_inst.regarray[2], uut.regfile_inst.regarray[14]);
-        @(posedge clk);
-        $display("%d:\t%d\t%d\t%d\t%d", uut.pc, uut.regfile_inst.regarray[0], uut.regfile_inst.regarray[1], uut.regfile_inst.regarray[2], uut.regfile_inst.regarray[14]);
-        @(posedge clk);
-        $display("%d:\t%d\t%d\t%d\t%d", uut.pc, uut.regfile_inst.regarray[0], uut.regfile_inst.regarray[1], uut.regfile_inst.regarray[2], uut.regfile_inst.regarray[14]);
-        @(posedge clk);
-        $display("%d:\t%d\t%d\t%d\t%d", uut.pc, uut.regfile_inst.regarray[0], uut.regfile_inst.regarray[1], uut.regfile_inst.regarray[2], uut.regfile_inst.regarray[14]);
-        @(posedge clk);
-        $display("%d:\t%d\t%d\t%d\t%d", uut.pc, uut.regfile_inst.regarray[0], uut.regfile_inst.regarray[1], uut.regfile_inst.regarray[2], uut.regfile_inst.regarray[14]);
-        @(posedge clk);
-        $display("%d:\t%d\t%d\t%d\t%d", uut.pc, uut.regfile_inst.regarray[0], uut.regfile_inst.regarray[1], uut.regfile_inst.regarray[2], uut.regfile_inst.regarray[14]);
-        @(posedge clk);
-        $display("%d:\t%d\t%d\t%d\t%d", uut.pc, uut.regfile_inst.regarray[0], uut.regfile_inst.regarray[1], uut.regfile_inst.regarray[2], uut.regfile_inst.regarray[14]);
-        @(posedge clk);
-        $display("%d:\t%d\t%d\t%d\t%d", uut.pc, uut.regfile_inst.regarray[0], uut.regfile_inst.regarray[1], uut.regfile_inst.regarray[2], uut.regfile_inst.regarray[14]);
-        @(posedge clk);
-        $display("%d:\t%d\t%d\t%d\t%d", uut.pc, uut.regfile_inst.regarray[0], uut.regfile_inst.regarray[1], uut.regfile_inst.regarray[2], uut.regfile_inst.regarray[14]);
-        @(posedge clk);
-        $display("%d:\t%d\t%d\t%d\t%d", uut.pc, uut.regfile_inst.regarray[0], uut.regfile_inst.regarray[1], uut.regfile_inst.regarray[2], uut.regfile_inst.regarray[14]);
+            uut.regfile_inst.regarray[0] = i; // Load some initial values into register as input
 
-        
-        // @(posedge clk);
-        // $display("%d", $signed(uut.regfile_inst.regarray[0]));
-        // @(posedge clk);
-        // $display("%d", $signed(uut.regfile_inst.regarray[0]));
-        // @(posedge clk);
-        // $display("%d", $signed(uut.regfile_inst.regarray[1]));
-        // @(posedge clk);
-        // $display("%d", $signed(uut.regfile_inst.regarray[2]));
-        // @(posedge clk);
-        // $display("%d", $signed(uut.regfile_inst.regarray[2]));
-        // @(posedge clk);
-        // $display("%d", $signed(uut.regfile_inst.regarray[2]));
-        // @(posedge clk);
-        // $display("%d", uut.regfile_inst.regarray[2]);
-        // @(posedge clk);
-        // $display("%d", $signed(uut.regfile_inst.regarray[2]));
-        // @(posedge clk);
-        // $display("%d", $signed(uut.regfile_inst.regarray[2]));
-        // @(posedge clk);
-        // $display("%d", $signed(uut.regfile_inst.regarray[2]));
-        // @(posedge clk);
-        // $display("%d", $signed(uut.regfile_inst.regarray[2]));
-        // @(posedge clk);
-        // $display("%d", $signed(uut.regfile_inst.regarray[2]));
-        // @(posedge clk);
-        // $display("%d", $signed(uut.regfile_inst.regarray[2]));
-        // @(posedge clk);
-        // $display("%d", $signed(uut.regfile_inst.regarray[2]));
-        // @(posedge clk);
-        // $display("%d", $signed(uut.regfile_inst.regarray[2]));
-        // @(posedge clk);
-        // $display("%d", $signed(uut.regfile_inst.regarray[2]));
-        // $display("%b", uut.alu_op);
-        
-        // #100;
+            prev_pc = -4;
+            prev_prev_pc = -8;
+            // If PC is the same as it was 2 cycles ago, we've hit an infinite loop
+            while (uut.pc != prev_prev_pc) begin
+                prev_prev_pc = prev_pc;
+                prev_pc = uut.pc;
+                @(posedge clk);
+            end
+            // $display("%d:\t%d\t%d\t%d\t%d", uut.pc, uut.regfile_inst.regarray[0], uut.regfile_inst.regarray[1], uut.regfile_inst.regarray[2], uut.regfile_inst.regarray[3]);
+            if (uut.regfile_inst.regarray[1] != i) begin
+                $display("Test failed for input %d: expected %d, got %d", i, i, uut.regfile_inst.regarray[1]);
+            end
+        end
         
         $display("Simulation finished. Check waveforms for results.");
         $finish;
---
+++ plan.md
@@ -0,0 +1,3 @@
+Binary search assembly: workloads/binsearch/binsearch.s
+Binary search imem file: workloads/binsearch/imem.mem
+Binary search dmem file: workloads/binsearch/dmem.mem
