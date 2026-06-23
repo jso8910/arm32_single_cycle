b63195f5ebf5bddbdeb505f422e4bccb6565b3a0
Jason Rosenzweig <mail4jasonr@gmail.com>
2026-06-23 12:15:34 -0500

    uncommented core

--- core.v
+++ core.v
@@ -1,161 +1,161 @@
 `timescale 1ns / 1ps
 `include "params.vh"
 
-// module arm32_core(
-//     input clk,
-//     input rst,
-//     input [`IWIDTH-1:0]     inst,
-//     input [`DWIDTH-1:0]     dram_dout,
-//     input [`WWIDTH*16-1:0]  bulk_data_sram,
-//     input [`AWIDTH-1:0]     bulkAddrResult,
-//     input [`WWIDTH-1:0]     op_a, reg_b, op_c, op_d,
+module arm32_core(
+    input clk,
+    input rst,
+    input [`IWIDTH-1:0]     inst,
+    input [`DWIDTH-1:0]     dram_dout,
+    input [`WWIDTH*16-1:0]  bulk_data_sram,
+    input [`AWIDTH-1:0]     bulkAddrResult,
+    input [`WWIDTH-1:0]     op_a, reg_b, op_c, op_d,
 
-//     // Regfile inputs
+    // Regfile inputs
 
-//     output                  condition_fulfilled,
-//     // DMEM controls
-//     output                  cs_data,
-//     output [2:0]            load_addressing_mode,
-//     output                  bulk_store_enable,
-//     output                  bulk_load_enable,
-//     output [15:0]           bulk_reglist,
-//     output                  we_data,
+    output                  condition_fulfilled,
+    // DMEM controls
+    output                  cs_data,
+    output [2:0]            load_addressing_mode,
+    output                  bulk_store_enable,
+    output                  bulk_load_enable,
+    output [15:0]           bulk_reglist,
+    output                  we_data,
 
-//     // Operand outputs
+    // Operand outputs
 
-//     // ALU outputs
-//     output [`DWIDTH-1:0]    alu_out_1, alu_out_2,
+    // ALU outputs
+    output [`DWIDTH-1:0]    alu_out_1, alu_out_2,
 
-//     // Regfile control signals
-//     output [3:0]            rd_addr_a, rd_addr_b, rd_addr_c, rd_addr_d,
-//     output [3:0]            wr_addr_a, wr_addr_b,
-//     output                  wr_en_a, wr_en_b,
-//     output [3:0]            pc_offset,
-//     output [`WWIDTH-1:0]    next_pc_seq,
-//     output                  bulk_writeback,
-//     output [1:0]            address_gen_mode,
-//     output                  mem_op,
+    // Regfile control signals
+    output [3:0]            rd_addr_a, rd_addr_b, rd_addr_c, rd_addr_d,
+    output [3:0]            wr_addr_a, wr_addr_b,
+    output                  wr_en_a, wr_en_b,
+    output [3:0]            pc_offset,
+    output [`WWIDTH-1:0]    next_pc_seq,
+    output                  bulk_writeback,
+    output [1:0]            address_gen_mode,
+    output                  mem_op,
 
-//     // IMEM controls
-//     output [`AWIDTH-1:0] pc
+    // IMEM controls
+    output [`AWIDTH-1:0] pc
 
-// );
-//     // Net declarations
-//     // ---Fetch
-//     wire [`WWIDTH-1:0] this_pc;
-//     wire [`WWIDTH-1:0] next_pc;
+);
+    // Net declarations
+    // ---Fetch
+    wire [`WWIDTH-1:0] this_pc;
+    wire [`WWIDTH-1:0] next_pc;
 
-//     // ---Decoder
-//     wire imm_flag;
-//     wire pc_op;
-//     wire set_cpsr;
-//     wire [3:0] condition_code;
-//     wire [1:0] op2_shift_func;
-//     wire [7:0] op2_imm_shift_by;
-//     wire op2_is_imm_shift;
-//     wire [`WWIDTH-1:0] imm_val;
-//     wire mem_op;
-//     wire [4:0] alu_op;
+    // ---Decoder
+    wire imm_flag;
+    wire pc_op;
+    wire set_cpsr;
+    wire [3:0] condition_code;
+    wire [1:0] op2_shift_func;
+    wire [7:0] op2_imm_shift_by;
+    wire op2_is_imm_shift;
+    wire [`WWIDTH-1:0] imm_val;
+    wire mem_op;
+    wire [4:0] alu_op;
 
-//     //--ALU
-//     wire [`WWIDTH-1:0] op_b;
-//     reg [3:0] prev_nzcv;
-//     wire shifter_cout;
-//     wire [3:0] nzcv;
-//     // External memory control signals
-//     assign pc = this_pc;
+    //--ALU
+    wire [`WWIDTH-1:0] op_b;
+    reg [3:0] prev_nzcv;
+    wire shifter_cout;
+    wire [3:0] nzcv;
+    // External memory control signals
+    assign pc = this_pc;
 
-//     // Fetch logic
+    // Fetch logic
 
-//     nextPcLogic pcgen(
-//         .this_pc(this_pc),
-//         .pc_mod(bulk_load_enable ? bulk_data_sram[15*32 +: 32] : alu_out_1),
-//         .pc_write(pc_op & condition_fulfilled),
-//         .next_pc(next_pc),
-//         .next_pc_seq(next_pc_seq)
-//     );
+    nextPcLogic pcgen(
+        .this_pc(this_pc),
+        .pc_mod(bulk_load_enable ? bulk_data_sram[15*32 +: 32] : alu_out_1),
+        .pc_write(pc_op & condition_fulfilled),
+        .next_pc(next_pc),
+        .next_pc_seq(next_pc_seq)
+    );
 
-//     fetchLogic fetch(
-//         .clk(clk),
-//         .rst(rst),
-//         // We want to select the value from SRAM 
-//         .next_pc(next_pc),
-//         .this_pc(this_pc)
-//     );
+    fetchLogic fetch(
+        .clk(clk),
+        .rst(rst),
+        // We want to select the value from SRAM 
+        .next_pc(next_pc),
+        .this_pc(this_pc)
+    );
 
-//     // Shifter
-//     register_shift_unit register_shift_unit_op2 (
-//         .in(imm_flag ? imm_val : reg_b),
-//         .func(op2_shift_func),
-//         .amount(op2_is_imm_shift ? op2_imm_shift_by : op_c[7:0]),
-//         .is_imm_rot(imm_flag),
-//         .cin(prev_nzcv[1]),
-//         .result(op_b),
-//         .cout(shifter_cout)
-//     );
+    // Shifter
+    register_shift_unit register_shift_unit_op2 (
+        .in(imm_flag ? imm_val : reg_b),
+        .func(op2_shift_func),
+        .amount(op2_is_imm_shift ? op2_imm_shift_by : op_c[7:0]),
+        .is_imm_rot(imm_flag),
+        .cin(prev_nzcv[1]),
+        .result(op_b),
+        .cout(shifter_cout)
+    );
 
-//     // ALU definitions
-//     alu alu_inst (
-//         .op_a(op_a),
-//         .op_b(op_b),
-//         .op_c(op_c),
-//         .op_d(op_d),
-//         .func(alu_op),
-//         .prev_nzcv(prev_nzcv),
-//         .shifter_cout(shifter_cout),
-//         .alu_out_1(alu_out_1),
-//         .alu_out_2(alu_out_2),
-//         .nzcv(nzcv)
-//     );
+    // ALU definitions
+    alu alu_inst (
+        .op_a(op_a),
+        .op_b(op_b),
+        .op_c(op_c),
+        .op_d(op_d),
+        .func(alu_op),
+        .prev_nzcv(prev_nzcv),
+        .shifter_cout(shifter_cout),
+        .alu_out_1(alu_out_1),
+        .alu_out_2(alu_out_2),
+        .nzcv(nzcv)
+    );
 
-//     // Conditional unit
-//     condition_unit cond(
-//         .nzcv(prev_nzcv),
-//         .condition_code(condition_code),
-//         .cond_met(condition_fulfilled)
-//     );
+    // Conditional unit
+    condition_unit cond(
+        .nzcv(prev_nzcv),
+        .condition_code(condition_code),
+        .cond_met(condition_fulfilled)
+    );
 
-//     // Decoder definitions
+    // Decoder definitions
 
-//     decode decode_inst (
-//         .inst(inst),
-//         .next_pc(next_pc_seq),
-//         .cs_data(cs_data),
-//         .imm_flag(imm_flag),
-//         .we_data(we_data),
-//         .wr_en_a(wr_en_a),
-//         .wr_en_b(wr_en_b),
-//         .pc_op(pc_op),
-//         .set_cpsr(set_cpsr),
-//         .condition_code(condition_code),
-//         .alu_op(alu_op),
-//         .wr_addr_a(wr_addr_a),
-//         .wr_addr_b(wr_addr_b),
-//         .rd_addr_a(rd_addr_a),
-//         .rd_addr_b(rd_addr_b),
-//         .rd_addr_c(rd_addr_c),
-//         .rd_addr_d(rd_addr_d),
-//         .op2_shift_func(op2_shift_func),
-//         .op2_imm_shift_by(op2_imm_shift_by),
-//         .op2_is_imm_shift(op2_is_imm_shift),
-//         .imm_val(imm_val),
-//         .address_gen_mode(address_gen_mode),
-//         .load_addressing_mode(load_addressing_mode),
-//         .mem_op(mem_op),
-//         .pc_offset(pc_offset),
-//         .bulk_store_enable(bulk_store_enable),
-//         .bulk_load_enable(bulk_load_enable),
-//         .bulk_reglist(bulk_reglist),
-//         .bulk_writeback(bulk_writeback)
-//     );
+    decode decode_inst (
+        .inst(inst),
+        .next_pc(next_pc_seq),
+        .cs_data(cs_data),
+        .imm_flag(imm_flag),
+        .we_data(we_data),
+        .wr_en_a(wr_en_a),
+        .wr_en_b(wr_en_b),
+        .pc_op(pc_op),
+        .set_cpsr(set_cpsr),
+        .condition_code(condition_code),
+        .alu_op(alu_op),
+        .wr_addr_a(wr_addr_a),
+        .wr_addr_b(wr_addr_b),
+        .rd_addr_a(rd_addr_a),
+        .rd_addr_b(rd_addr_b),
+        .rd_addr_c(rd_addr_c),
+        .rd_addr_d(rd_addr_d),
+        .op2_shift_func(op2_shift_func),
+        .op2_imm_shift_by(op2_imm_shift_by),
+        .op2_is_imm_shift(op2_is_imm_shift),
+        .imm_val(imm_val),
+        .address_gen_mode(address_gen_mode),
+        .load_addressing_mode(load_addressing_mode),
+        .mem_op(mem_op),
+        .pc_offset(pc_offset),
+        .bulk_store_enable(bulk_store_enable),
+        .bulk_load_enable(bulk_load_enable),
+        .bulk_reglist(bulk_reglist),
+        .bulk_writeback(bulk_writeback)
+    );
 
-//     always @(posedge clk or negedge rst) begin
-//         if (!rst) begin
-//             prev_nzcv <= 4'b0000;
-//         end else if (set_cpsr)
-//             prev_nzcv <= nzcv;
-//     end
-// endmodule
+    always @(posedge clk or negedge rst) begin
+        if (!rst) begin
+            prev_nzcv <= 4'b0000;
+        end else if (set_cpsr)
+            prev_nzcv <= nzcv;
+    end
+endmodule
 
 module arm32_core_system(
     input clk,
