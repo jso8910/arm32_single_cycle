b9274d24e2fe0f141b9dc7c9a4a5529dfb9dcace
Jason Rosenzweig <mail4jasonr@gmail.com>
2026-06-23 22:44:41 -0500

    new syntehsis script

--- synthesis.tcl
+++ synthesis.tcl
@@ -2,6 +2,7 @@ file mkdir "../profiling"
 file mkdir "../outputs"
 
 set all_files [glob -nocomplain ../*.v ../*.sv]
+set core_files [lsort [glob -nocomplain ../cores/*.v ../cores/*.sv]]
 
 set filtered_files {}
 foreach file $all_files {
@@ -19,18 +20,49 @@ foreach file $all_files {
     }
 }
 
-puts "RTL files being synthesized:"
-puts [join $filtered_files "\n"]
+proc get_top_module {hdl_file} {
+    set file_handle [open $hdl_file r]
+
+    while {[gets $file_handle line] >= 0} {
+        if {[regexp {^\s*module\s+([A-Za-z_][A-Za-z0-9_$]*)} $line -> module_name]} {
+            close $file_handle
+            return $module_name
+        }
+    }
+
+    close $file_handle
+    error "No module declaration found in $hdl_file"
+}
+
+set synthesis_targets [list \
+    [list arm32_core $filtered_files ../arm32_core.sdc] \
+    [list regfile $filtered_files ../regfile.sdc] \
+]
+
+foreach core_file $core_files {
+    set top_module [get_top_module $core_file]
+    lappend synthesis_targets [list $top_module [list $core_file] ../arm32_core.sdc]
+}
+
+puts "Synthesis targets:"
+foreach target $synthesis_targets {
+    lassign $target top_module source_files sdc_file
+    puts "$top_module:"
+    puts "  RTL: [join $source_files {, }]"
+    puts "  SDC: $sdc_file"
+}
 
 set_db library /vol/ece303/genus_tutorial/NangateOpenCellLibrary_typical.lib
 set_db lef_library /vol/ece303/genus_tutorial/NangateOpenCellLibrary.lef
 
-foreach top_module {arm32_core regfile} {
-    read_hdl -sv $filtered_files
+foreach target $synthesis_targets {
+    lassign $target top_module source_files sdc_file
+
+    read_hdl -sv $source_files
     elaborate $top_module
     current_design $top_module
 
-    read_sdc ../${top_module}.sdc
+    read_sdc $sdc_file
 
     syn_generic
     syn_map
